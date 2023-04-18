0ee88ca60ae9fe4c94986d53296763089fe1b794
Pedro Leal <pedropeter93.pl@gmail.com>
2023-04-18 00:19:33 -0300

    Anexo de las funciones de encriptado y copiado

---
+++ script.js
@@ -0,0 +1,51 @@
+const botonEncriptado = document.getElementById("boton-encriptado");
+const botonCopiar = document.getElementById("boton-copiar");
+const botonDesencriptar = document.getElementById("boton-desencriptado");
+
+// Función para encriptar.
+botonEncriptado.addEventListener("click",function encriptacion()
+{
+    const textoPorEncriptar = document.getElementById("textoporencriptar");
+    const textoSinEncriptar = textoPorEncriptar.value;
+    
+    // Proceso para encriptar.
+    const textoConEnter = textoSinEncriptar.replaceAll('e', 'enter');
+    const textoConImes = textoConEnter.replaceAll('i', 'imes');
+    const textoConAi = textoConImes.replaceAll('a', 'ai');
+    const textoConOber = textoConAi.replaceAll('o', 'ober');
+    const textoFinal = textoConOber.replaceAll('u', 'ufat');
+
+    document.querySelector(".respuesta figure").remove();
+    document.querySelector(".respuesta h3").remove();
+
+    document.querySelector('.respuesta p').innerHTML = "";
+    document.querySelector('.respuesta p').innerHTML = textoFinal;
+    document.querySelector('.copiar').style.display = "inline-block";
+    document.querySelector('.bloque-respuesta').style.justifyContent = "space-between";
+    document.querySelector('.bloque-respuesta p').style.margin = "4rem 3rem";
+    document.querySelector('.bloque-respuesta .copiar').style.marginBottom = "2rem";
+})
+
+// Funcion para copiar.
+botonCopiar.addEventListener("click",function copiar()
+{
+    const texto = document.querySelector('.respuesta p');
+    // Crear un elemento temporal porque es necesario almacenar ese valor en la memoria (el usuario no lo ve).
+    const textareaTemporal = document.createElement('textarea');
+
+    // Luego establecer su valor al contenido del elemento seleccionado
+    textareaTemporal.value = texto.textContent;
+
+    // Agregar el elemento temporal al documento, es decir a la memoria del naveagdor (el usuario no lo ve).
+    document.body.appendChild(textareaTemporal);
+
+    // Seleccionar el elemento temporal
+    textareaTemporal.select();
+
+    // Luego copiar su contenido a la portapapeles
+    document.execCommand('copy');
+
+    // Eliminar el elemento temporal del documento y liberar el espacio de memoria utilizado en el navegador.
+    document.body.removeChild(textareaTemporal);
+})
+
